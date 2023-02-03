7148e8dc5c07d5091f021961e8d03a6da35a9ef1
yuehua-HNU <1538786700@qq.com>
2023-02-03 19:57:55 +0800

    Site updated: 2023-02-03 19:57:52

--- js/title.js
+++ js/title.js
@@ -4,14 +4,14 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '只因你太美~~';
+    document.title = 'Do you ?';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '喜欢唱、跳、rap、篮球';
+    document.title = '我爱这个魔幻的世界';
     //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
+    // titleTime = setTimeout(function () {
+    //   document.title = OriginTitile;
+    // }, 2000);
   }
 });
